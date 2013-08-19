87780911d4126cf9a8c57274731fbb33527ceaee
Patrick McElwee <pmm21@duke.edu>
2013-08-18 23:08:50 -0400

    Avoid Phrase.phrase, use scan instead of split Remove 'downcased', an implementation detail from method name

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,19 +1,18 @@
 class Phrase
-  attr_reader :phrase
-
   def initialize(phrase)
     @phrase = phrase
   end
 
   def word_count
-    downcased_words.each_with_object(Hash.new(0)) do |word, count|
-      count[word] += 1
+    words.each_with_object(Hash.new(0)) do |word, counts|
+      counts[word] += 1
     end
   end
 
   private
+  attr_reader :phrase
 
-  def downcased_words
-    phrase.downcase.split(/\W+/)
+  def words
+    phrase.downcase.scan(/\w+/)
   end
 end
